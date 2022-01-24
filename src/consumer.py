43403ea028933c925c69788576cc6eb35ec0e6c3
medinazorin_desk <medinazorin@gmail.com>
2022-01-24 18:13:46 +0100

    req update

--- src/consumer.py
+++ src/consumer.py
@@ -1,16 +1,35 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from json import loads
-from time import sleep
+from json import dumps
+from geopy.distance import geodesic
 consumer = KafkaConsumer(
-    'topic_test',
+    'generator',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='my-group-id',
     value_deserializer=lambda x: loads(x.decode('utf-8'))
 )
+# producer = KafkaProducer(
+#     bootstrap_servers=['localhost:9092'],
+#     value_serializer=lambda x: dumps(x).encode('utf-8')
+# )
+
+
+def nearby_friends(pos_user,pos_friend):
+    pos_user = (float(pos_user['lat']), float(pos_user['lon']))
+    pos_friend = (float(pos_friend['lat']), float(pos_friend['lon']))
+    if geodesic(pos_user, pos_friend).meters <= 100:
+        # Send data
+        print('MATCH')
+
+def get_matches(dc):
+    for val in dc.values():
+        id, friends, position = val['id'],val['friends'], val['position']
+        for k in friends:
+            nearby_friends(position, dc[k]['position'])
+
 for event in consumer:
-    event_data = event.value
-    # Do whatever you want
+    event_data = original_data = event.value
     print(event_data)
-    # sleep(2)
+    get_matches(event_data)
