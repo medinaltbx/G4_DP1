2e945ee76e50ab120b1c804361254abb5102aa7b
cmedina <crimeaz.forja@gmail.com>
2022-01-27 10:18:01 +0100

    Added automatic table creation postgres

---
+++ docker/docker_postgres_with_data/sql/create_tables.sql
@@ -0,0 +1,25 @@
+-- Creation of raw_data table
+CREATE TABLE IF NOT EXISTS raw_data (
+	id INT ,
+	name VARCHAR ( 50 ),
+    last_name VARCHAR ( 50 ),
+    f1 INT,
+    f2 INT,
+    f3 INT,
+    f4 INT,
+    f5 INT,
+    f6 INT,
+    f7 INT,
+    f8 INT,
+    f9 INT,
+    f10 INT,
+    lat FLOAT,
+    lon FLOAT,
+    transport VARCHAR ( 50 ),
+    age INT,
+    gender VARCHAR ( 5 ),
+    weight FLOAT,
+    height FLOAT,
+	time TIMESTAMP,
+    PRIMARY KEY (id, time)
+);
--- docker/postgres_docker_v3/world.sql
+++
@@ -1,6 +0,0 @@
-USE metaverso;
-CREATE TABLE raw_data (
-	user_id INT PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-);
